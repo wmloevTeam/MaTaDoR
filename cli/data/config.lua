108a5cb24c9168336ff1b2e1567285de8d1a2b81
wmloevTeam <37336493+wmloevTeam@users.noreply.github.com>
2018-03-13 16:55:16 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,56 @@
+do local _ = {
+  admins = {
+    {
+      307905718,
+      "@Kiana1378"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore",
+    "Msg",
+    "BanHammer",
+    "Administrative",
+    "Help",
+    "Limitmember",
+    "Rank",
+    "Openchat",
+    "Del-Chat",
+    "Monshi",
+    "Rank",
+    "Warn",
+    "Auto-Lock",
+    "Plugins",
+    "Fuck",
+    "cleanmembers",
+    "Zarinpal",
+    "Fun",
+    "addkick"
+  },
+  moderation = {
+    data = "./data/moderation.json",
+    tabgood = "./data/tabgood.json"
+  },
+  sudo_users = {
+    474987896,
+    490443760,
+    385486291,
+    413678331,
+    425451627,
+    464123002,
+    117676659,
+    258139777,
+    136957484,
+    472881554,
+    400126262,
+    374205890,
+    492881150,
+    114740646,
+    366324943,
+    419931714,
+    185520064,
+    97476012
+  }
+}
+return _
+end
