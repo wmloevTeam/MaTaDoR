a7b19cb6ab04227956f22c29dc4eb0a179281963
wmloevTeam <37336493+wmloevTeam@users.noreply.github.com>
2018-03-13 16:47:57 +0330

    Add files via upload

---
+++ api/data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore"
+  },
+  moderation = {
+    data = "/home/matador/MaTaDoR/cli/data/moderation.json"
+  },
+  sudo_users = {
+    490443760,
+    474987896,
+    97476012
+  }
+}
+return _
+end
